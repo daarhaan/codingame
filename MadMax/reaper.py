10a9feee74981ce0d0c47bd01c9cd2397c6da457
daarhaan <marius.stan@gmail.com>
2017-11-17 18:54:45 +0100

    added reaper file

---
+++ MadMax/reaper.py
@@ -0,0 +1,36 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# game loop
+while True:
+    my_score = int(input())
+    enemy_score_1 = int(input())
+    enemy_score_2 = int(input())
+    my_rage = int(input())
+    enemy_rage_1 = int(input())
+    enemy_rage_2 = int(input())
+    unit_count = int(input())
+    for i in range(unit_count):
+        unit_id, unit_type, player, mass, radius, x, y, vx, vy, extra, extra_2 = input().split()
+        unit_id = int(unit_id)
+        unit_type = int(unit_type)
+        player = int(player)
+        mass = float(mass)
+        radius = int(radius)
+        x = int(x)
+        y = int(y)
+        vx = int(vx)
+        vy = int(vy)
+        extra = int(extra)
+        extra_2 = int(extra_2)
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+
+    print("WAIT")
+    print("WAIT")
+    print("WAIT")
